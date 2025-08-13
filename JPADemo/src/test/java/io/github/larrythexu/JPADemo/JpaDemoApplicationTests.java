56ab49ffbfb9a6ef0c0065e450f618823c80f879
lxu <lawrencexu8@gmail.com>
2025-08-13 16:48:44 -0400

    setting up jpa demo

---
+++ JPADemo/src/test/java/io/github/larrythexu/JPADemo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.larrythexu.JPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
